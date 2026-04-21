d53a8428360c4580d662f4ab11f2052a5703e9fe
Adrian Fuster <adrianfuster@elcampico.org>
2026-04-21 17:23:22 +0200

    Fin

--- js/main.js
+++ js/main.js
@@ -1,55 +1,72 @@
-// Definicion de las constantes
+// Definición de constantes y enlace al DOM
 const listaPokemon = document.querySelector("#listaPokemon");
-const botonesHeader = document.querySelector(".btn-header");
+const botonesHeader = document.querySelectorAll(".btn-header");
 
 // Enlace a la API
 
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
-// Peticion API
+// Peticion a la API
 
-for(let i=0; i<=151;i++){
+for(let i = 1; i <= 151; i++){
     fetch(URL + i)
         .then((response) => response.json()
         .then((data) => mostrarPokemon(data)))
 }
 
-// Funcion de mostrar pokemon
-
 function mostrarPokemon(data){
-    let tipos = data.types.map((type) => 
-        `<p class="${type.type.name} tipo"> ${type.type.name}</p>`);
 
+    let tipos = data.types.map((type) => `<p class="${type.type.name} tipo"> ${type.type.name}</p>`);
     tipos = tipos.join('');
 
     let pokeId = data.id.toString();
     if(pokeId.length === 1){
         pokeId = "00" + pokeId;
-    } else if(pokeId.length === 2){
+    }else if(pokeId.length === 2){
         pokeId = "0" + pokeId;
     }
 
     const div = document.createElement("div");
     div.classList.add("pokemon");
-    div.innerHTML = 
-        `<p class="pokemon-id-back"> ${pokeId} </p>
-         <div class="pokemon-imagen">
-            <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
-         </div>
-         <div class="pokemon-info">
-            <div class="nombre-contenedor">
-                <p class="pokemon-id">${pokeId}</p>
-                <h2 class="pokemon-nombre">${data.name}</h2>
-                </div>
-                <div class="pokemon-tipos">
-                    ${tipos}
-                </div>
-                <div class="pokemon-stats">
-                    <p class="stat">${data.height}M</p>
-                    <p class="stat">${data.weight}KG</p>
-                </div>
-                </div>`
-
+    div.innerHTML = `<p class="pokemon-id-back">${pokeId}</p>
+                    <div class="pokemon-imagen">
+                        <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}">
+                    </div>
+                    <div class="pokemon-info">
+                        <div class="nombre-contenedor">
+                            <p class="pokemon-id">${pokeId}</p>
+                            <h2 class="pokemon-nombre">${data.name}</h2>
+                        </div>
+                        <div class="pokemon-tipos">
+                            ${tipos}
+                        </div>
+                        <div class="pokemon-stats">
+                            <p class="stat">${data.height} M</p>
+                            <p class="stat">${data.weight} KG</p>
+                        </div>
+                    </div>`
+                ;
     listaPokemon.append(div);
 }
 
+// funcionalidad Header
+
+botonesHeader.forEach( boton => boton.addEventListener("click", (event) =>{
+    const botonId = event.currentTarget.id;
+
+    listaPokemon.innerHTML = "";
+    for(let i = 1; i<= 151; i++){
+        fetch(URL + i)
+            .then((response) => response.json())
+            .then((data) => {
+                if(botonId === "ver-todos"){
+                    mostrarPokemon(data);
+                }else{
+                    const tipos = data.types.map(type => type.type.name);
+                    if(tipos.some(tipo => tipo.includes(botonId))){
+                        mostrarPokemon(data);
+                    }
+                }
+            })
+    }
+}))
